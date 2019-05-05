e4b65a4cc090c4190f2dd545d5e16f079bf8a250
Manga Estranha <mangaestranha@localhost.localdomain>
2019-05-05 15:33:39 -0300

    Updates

---
+++ precache-manifest.e650f5aa846e8436a1c76de26fc68bae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1922700f919b2843157ecc9598305af2",
+    "url": "/do-to-list-react-app/index.html"
+  },
+  {
+    "revision": "b85c50b43c759aed75ae",
+    "url": "/do-to-list-react-app/static/css/main.21c294f0.chunk.css"
+  },
+  {
+    "revision": "123b1c7fbd2a84f1159a",
+    "url": "/do-to-list-react-app/static/js/2.1d24cf50.chunk.js"
+  },
+  {
+    "revision": "b85c50b43c759aed75ae",
+    "url": "/do-to-list-react-app/static/js/main.1f12ccca.chunk.js"
+  },
+  {
+    "revision": "83bea3d219655a5fb6ad",
+    "url": "/do-to-list-react-app/static/js/runtime~main.e4703fa3.js"
+  }
+]);
